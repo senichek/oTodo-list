d28acc197608ca96af3e49c938cfc4f5502a0975
senichek <senichek@gmail.com>
2023-01-07 20:20:01 +0100

    corrected typo in database js file

--- app/database.js
+++ app/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: env.DB_HOST,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
